300ef121898fa577a06103f2c226f53293d73836
mvill <119754846+migavg@users.noreply.github.com>
2023-06-23 23:01:30 -0700

    added hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import style from "./style.css";
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
+    <div>
     <Navbar/>
+    <Hero/>
+    </div>
   );
 }
 
