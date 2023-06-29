5f9bd119c50162753f949919fb2e064fbdf0bf5c
mvill <119754846+migavg@users.noreply.github.com>
2023-06-28 22:23:41 -0700

    added basic container to page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import style from "./style.css";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Container from './components/Container';
 
 function App() {
   return (
     <div>
     <Navbar/>
     <Hero/>
+    <Container/>
     </div>
   );
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -4,8 +4,8 @@ import React, {useState} from "react";
 const Container = () => {
    
       return (
-        <div>
-       
+        <div className="container">
+       <p>This is my main Container</p>
     
         </div>
     
